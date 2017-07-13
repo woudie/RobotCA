3da210d5fd1fb359d3f1dcf639f058155593bebb
dkati <dkatikaridis@gmail.com>
2017-07-13 12:11:35 +0300

    build.sh:Install app to device if build successful

--- build.sh
+++ build.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
 BUILD_DIR=src/android_foo
-
+APK_DIR=control_app/build/outputs/apk
+APK_NAME=control_app-debug.apk
 
 build() {
 	cd $BUILD_DIR
 	./gradlew build -x lint
+	if [ -e  $APK_DIR/$APK_NAME ]; then
+		read -t 3 -p "Install app?, 3 sec timeout (y/n)";
+		if [ "$REPLY" == "y" ]; then
+			cd $APK_DIR
+			adb install -r $APK_NAME
+			echo "App installed"
+			adb shell am start -n com.robotca.ControlApp/com.robotca.ControlApp.SplashActivity
+			cd ../../..
+		fi;
+	else
+		echo "Build failed!!!!"
+	fi;
 	cd ../..
 }
 
@@ -48,5 +61,3 @@ select build in "Build" "Build with debug" "Clean" "Exit"; do
 	esac
 done
 exit 0
-
-. src/android_foo/gradlew build
